f1b174da72e437124311057df1c7c948fb69249f
hudsonmcmanus <hudson.mcmanus@gmail.com>
2020-06-12 21:38:07 -0700

    Changed admin for realtors and finished about page

--- realtors/admin.py
+++ realtors/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import Realtor
 
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display = ('id', 'name', 'is_mvp', 'email', 'hire_date')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
     list_per_page = 25
-
+    list_editable = ('is_mvp',)
 
 admin.site.register(Realtor, RealtorAdmin)
