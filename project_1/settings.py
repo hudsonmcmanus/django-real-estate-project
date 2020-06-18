2c7b268003fe3c3b08f2d0465e3de9adc4692a2b
hudsonmcmanus <hudson.mcmanus@gmail.com>
2020-06-18 01:19:47 -0700

    Removed email from settings

--- project_1/settings.py
+++ project_1/settings.py
@@ -142,14 +142,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email Config
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'projectrealestatetest@gmail.com'
-EMAIL_HOST_PASSWORD = 'djangotest'
-
 try:
     from .local_settings import *
 except ImportError:
